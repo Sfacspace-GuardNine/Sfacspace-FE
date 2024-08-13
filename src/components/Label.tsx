364bee6d385ddc59963ba4f202de3f4172b30eb9
HYK-Nov <yundori97@gmail.com>
2024-08-13 19:01:11 +0900

    feat: Label 컴포넌트 생성

---
+++ src/components/Label.tsx
@@ -0,0 +1,32 @@
+import { ReactNode } from "react";
+
+type TProps = {
+  children: ReactNode;
+  variant: keyof typeof LabelColor;
+};
+
+const LabelColor = {
+  red: { bg: "#FFEAE4", text: "#FF3D00" },
+  blue: { bg: "#E4F2FF", text: "#4C93FF" },
+  green: { bg: "#DDFFF3", text: "#00987C" },
+  purple: { bg: "#F5E4FF", text: "#A54CFF" },
+  pink: { bg: "#FFF2F7", text: "#FF81A7" },
+  yellow: { bg: "#FFFBE4", text: "#FF8A00" },
+} as const;
+
+export default function Label({ children, variant }: TProps) {
+  return (
+    <>
+      <span
+        className={`rounded-full px-3 py-3 text-center text-xl font-medium outline outline-1`}
+        style={{
+          backgroundColor: LabelColor[variant].bg,
+          color: LabelColor[variant].text,
+          outlineColor: LabelColor[variant].text,
+        }}
+      >
+        {children}
+      </span>
+    </>
+  );
+}
