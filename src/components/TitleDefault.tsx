d9355522ab13e296679a13a4fa453c19f8054c53
HYK-Nov <yundori97@gmail.com>
2024-08-13 20:48:46 +0900

    feat: TitleDefault 컴포넌트 생성

---
+++ src/components/TitleDefault.tsx
@@ -0,0 +1,31 @@
+import { ReactNode } from "react";
+
+import Image from "next/image";
+import Link from "next/link";
+
+type TTitleDefaultProps = {
+  children: ReactNode;
+  link: string;
+};
+
+export default function TitleDefault({ children, link }: TTitleDefaultProps) {
+  return (
+    <>
+      <Link href={`${link}`}>
+        <div
+          className={
+            "flex items-center gap-6 rounded-full p-5 text-4xl font-normal text-primary-500 outline outline-4"
+          }
+        >
+          <Image
+            src={"./images/arrow_left.svg"}
+            alt={"arrow_left"}
+            width={36}
+            height={36}
+          />
+          {children}
+        </div>
+      </Link>
+    </>
+  );
+}
