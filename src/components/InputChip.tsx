0fffd0bac3d1f1cceadd99d6ced59aec7ff0b4bb
an hyeonjin <hyeonjinan096@naver.com>
2024-08-14 21:55:05 +0900

    feat: InputChip컴포넌트 구현

---
+++ src/components/InputChip.tsx
@@ -0,0 +1,69 @@
+import { HTMLAttributes } from "react";
+
+import Image from "next/image";
+
+import { cn } from "@/utils/cn";
+
+type TInputChipProps = {
+  text: string;
+  hasIcon?: boolean;
+  percentage?: number;
+  hasClose?: boolean;
+  variant?: "dark" | "light";
+  progress?: "0" | "1" | "2" | "3";
+} & HTMLAttributes<HTMLDivElement>;
+
+const variantClasses = {
+  dark: "bg-[#230C4E]",
+  light: "bg-[#7E5AFF]",
+};
+
+const progressClasses = {
+  "0": "bg-opacity-[0]",
+  "1": "bg-opacity-[.08]",
+  "2": "bg-opacity-[.12]",
+  "3": "bg-opacity-[.16]",
+};
+
+export const InputChip = ({
+  text,
+  hasIcon = true,
+  percentage,
+  hasClose = true,
+  className,
+  variant = "dark",
+  progress = "3",
+  ...props
+}: TInputChipProps) => {
+  return (
+    <div
+      className={cn(
+        "flex w-[221px] justify-between rounded-lg bg-[#230C4E] bg-opacity-[.44] px-[12px] py-[8px] text-[#3F3F3F] hover:shadow-lg",
+        variantClasses[variant],
+        progressClasses[progress],
+        className,
+      )}
+      {...props}
+    >
+      {hasIcon && (
+        <Image
+          src={"./images/document_icon.svg"}
+          alt={"file icon"}
+          width={24}
+          height={24}
+        />
+      )}
+      <span>{text}</span>
+      {percentage && <span>{percentage}%</span>}
+      {hasClose && (
+        <Image
+          src={"./images/X.svg"}
+          alt={"close icon"}
+          width={11.5}
+          height={11.5}
+          className="m-[2px]"
+        />
+      )}
+    </div>
+  );
+};
---
+++ src/stories/InputChip.stories.ts
@@ -0,0 +1,34 @@
+import { Meta, StoryObj } from "@storybook/react";
+
+import { InputChip } from "@/components/InputChip";
+
+const meta: Meta<typeof InputChip> = {
+  title: "components/InputChip",
+  tags: ["autodocs"],
+  component: InputChip,
+};
+
+export default meta;
+
+type Story = StoryObj<typeof InputChip>;
+
+export const Default: Story = {
+  args: {
+    children: "Default InputChip",
+    text: ".eslintrc.json",
+  },
+};
+
+export const Dark: Story = {
+  args: {
+    variant: "dark",
+    text: ".eslintrc.json",
+  },
+};
+
+export const Light: Story = {
+  args: {
+    variant: "light",
+    text: ".eslintrc.json",
+  },
+};
