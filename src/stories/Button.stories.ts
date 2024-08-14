7047303749243579c0218a5aa9d5796391a37a2f
an hyeonjin <hyeonjinan096@naver.com>
2024-08-14 22:32:35 +0900

    fix: Button storybook import 구문 수정

--- src/stories/Button.stories.ts
+++ src/stories/Button.stories.ts
@@ -1,6 +1,6 @@
 import { Meta, StoryObj } from "@storybook/react";
 
-import { Button } from "@/components/Button";
+import Button from "@/components/Button";
 
 const meta: Meta<typeof Button> = {
   title: "components/Button",
