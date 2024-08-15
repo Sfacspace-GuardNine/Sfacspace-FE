321ffa3f91cfbf0f5342837f689938e08686655b
ijjung365 <ijjung365@gmail.com>
2024-08-14 22:56:50 -0700

    feat: Input component 구현

---
+++ src/stories/Input.stories.ts
@@ -0,0 +1,20 @@
+import { Meta, StoryObj } from "@storybook/react";
+
+import Input from "@/components/Input";
+
+const meta: Meta<typeof Input> = {
+  title: "components/Input",
+  tags: ["autodocs"],
+  component: Input,
+};
+
+export default meta;
+
+type Story = StoryObj<typeof Input>;
+
+export const Default: Story = {
+  args: {
+    disabled: false,
+    variant: "",
+  },
+};
