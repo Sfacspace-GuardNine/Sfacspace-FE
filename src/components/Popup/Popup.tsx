6f6bcf8e8b3609030595f06a53ef6b05ce5225af
an hyeonjin <hyeonjinan096@naver.com>
2024-08-16 22:29:40 +0900

    feat: Popup컴포넌트 구현

---
+++ src/components/Popup/Popup.tsx
@@ -0,0 +1,121 @@
+import React, { PropsWithChildren } from "react";
+import { HTMLAttributes } from "react";
+
+import { createPortal } from "react-dom";
+
+import { cn } from "@/utils/cn";
+
+import Button from "../Button";
+
+interface TPopupProps {
+  isShow: boolean;
+  onClose?: () => void;
+  backgroundClassName?: string;
+  contentClassName?: string;
+
+  variant?: "large" | "small";
+  hasButtons?: boolean;
+  buttonText?: [string, string?];
+}
+
+function Title({
+  text,
+  className,
+  textSize = "sm",
+}: { textSize?: "sm" | "lg"; text: string } & HTMLAttributes<HTMLDivElement>) {
+  return (
+    <div
+      className={cn(
+        "flex justify-center py-[10px] font-semibold",
+        textSize === "sm" ? "text-xl" : "text-2xl",
+        className,
+      )}
+    >
+      <span>{text}</span>
+    </div>
+  );
+} //제목
+
+function Content({
+  text,
+  helpText,
+  className,
+}: { text: string; helpText?: string } & HTMLAttributes<HTMLDivElement>) {
+  return (
+    <div className={cn("text-[#969696]", className)}>
+      <div className="flex justify-center p-[10px] text-base">{text}</div>
+      {helpText && (
+        <div className="flex justify-center p-[10px] text-xs">{helpText}</div>
+      )}
+    </div>
+  );
+} //내용
+
+function Container({
+  isShow,
+  onClose,
+  backgroundClassName,
+  contentClassName,
+  children,
+  variant = "small",
+  hasButtons = true,
+  buttonText = ["button", "button"],
+}: PropsWithChildren<TPopupProps>) {
+  const variantClasses = {
+    large: "p-[48px] gap-[32px]",
+    small: "p-[32px] gap-[24px] shadow-[0_2px_12px_rgba(0,0,0,0.25)] ",
+  };
+
+  if (!isShow) {
+    return null;
+  }
+
+  return createPortal(
+    <div
+      className={cn(
+        "z-100 absolute bottom-0 left-0 right-0 top-0 flex w-full items-center justify-center",
+        backgroundClassName,
+      )}
+      onClick={onClose}
+    >
+      <div
+        className={cn(
+          "flex min-w-[380px] flex-col items-center justify-center rounded-[20px] bg-white",
+          variantClasses[variant],
+          contentClassName,
+        )}
+        onClick={(e) => e.stopPropagation()}
+      >
+        {children}
+        {hasButtons && (
+          <div className="flex gap-[12px]">
+            {variant === "small" && (
+              <Button shape="round" variant="fill" size="sm">
+                {buttonText[0]}
+              </Button>
+            )}
+            {variant === "large" && (
+              <>
+                <Button shape="square" variant="outline" size="sm">
+                  {buttonText[0]}
+                </Button>
+                <Button shape="square" variant="fill" size="sm">
+                  {buttonText[1]}
+                </Button>
+              </>
+            )}
+          </div>
+        )}
+      </div>
+    </div>,
+    document.body,
+  );
+}
+
+const Popup = {
+  Container,
+  Title,
+  Content,
+};
+
+export default Popup;
