a7dd3ac794557dd605748c192c9bec41cb3acd39
HYK-Nov <yundori97@gmail.com>
2024-08-16 18:22:40 +0900

    style: border, bg 컬러 수정

--- src/components/Switch.tsx
+++ src/components/Switch.tsx
@@ -12,7 +12,7 @@ export default function Switch({
   hasIcon = false,
   ...rest
 }: TSwitchProps) {
-  const switchClass = `${disabled ? "border-primary-300 bg-background-purpleLight peer-checked:bg-primary-300" : "bg-primary-50 active:border-[#ADADAD]"}`;
+  const switchClass = `${disabled ? "border-primary-200 bg-background-purpleLight peer-checked:bg-primary-200" : "bg-primary-50 active:border-[#ADADAD]"}`;
   const slideClass = `${hasIcon ? "h-6 w-6 top-1 left-1 bg-[url('/images/switch-x.svg')] peer-checked:bg-[url('/images/switch-check.svg')] peer-checked:translate-x-[20px]" : "h-4 w-4 left-2 top-[8px] peer-checked:translate-x-[16px]"} ${disabled ? "bg-[#1D1B20]" : "bg-[#79747E] active:bg-[#3F3F3F] hover:shadow-[0_0_0_12px_rgba(29,27,32,0.08)] hover:active:shadow-[0_0_0_12px_rgba(201,168,255,0.5)] peer-checked:hover:shadow-[0_0_0_12px_rgba(166,111,255,0.12)] active:scale-[1.1]"}`;
 
   return (
