77bbbd24bfdf5e3ed9b5ea7568dfc1b03f6e3297
JSKim <jskim4695@gmail.com>
2024-08-17 17:33:39 +0900

    fix: children 추가 및 buttonClass 코드 수정

--- src/components/Floating.tsx
+++ src/components/Floating.tsx
@@ -1,20 +1,27 @@
-import React, { ButtonHTMLAttributes } from "react";
+import React, { ButtonHTMLAttributes, ReactNode } from "react";
 
 import { cn } from "@/utils/cn";
 
 type TFloatingProps = {
   variant?: "ask" | "top";
+  children?: ReactNode;
 } & ButtonHTMLAttributes<HTMLButtonElement>;
 
-function Floating({ variant = "ask", className, ...props }: TFloatingProps) {
+function Floating({ variant, className, children, ...props }: TFloatingProps) {
   const buttonClass = cn(
     "h-[76px] w-[76px] bg-cover",
     variant === "ask"
       ? "bg-[url('/images/floating-ask-enabled.svg')] hover:bg-[url('/images/floating-ask-hovered.svg')]"
-      : "bg-[url('/images/floating-top-enabled.svg')] hover:bg-[url('/images/floating-top-hovered.svg')]",
+      : variant === "top"
+        ? "bg-[url('/images/floating-top-enabled.svg')] hover:bg-[url('/images/floating-top-hovered.svg')]"
+        : "",
   );
 
-  return <button className={cn(buttonClass, className)} {...props} />;
+  return (
+    <button className={cn(buttonClass, className)} {...props}>
+      {children}
+    </button>
+  );
 }
 
 export default Floating;
