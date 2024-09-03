354fe6e30f6ae1600bcbdf104e38212429820c45
an hyeonjin <hyeonjinan096@naver.com>
2024-09-03 23:55:06 +0900

    fix: 빌드 에러 수정

--- src/stories/OnlyCard.stories.ts
+++ src/stories/OnlyCard.stories.ts
@@ -16,7 +16,6 @@ export const Default: Story = {
   args: {
     title: "[취약성 경고] Microsoft의 여러 보안 취약점에 대한 CNNVD의 보고서",
     description: "여기에 설명이 들어갑니다.",
-    daysAgo: 3,
   },
 };
 
@@ -25,7 +24,6 @@ export const Elevated: Story = {
     variant: "elevated",
     title: "엘리베이티드 카드 제목",
     description: "엘리베이티드 상태의 설명입니다.",
-    daysAgo: 5,
   },
 };
 
@@ -34,7 +32,6 @@ export const Outlined: Story = {
     variant: "outlined",
     title: "아웃라인 카드 제목",
     description: "아웃라인 상태의 설명입니다.",
-    daysAgo: 10,
   },
 };
 
@@ -43,6 +40,5 @@ export const Filled: Story = {
     variant: "filled",
     title: "필드 카드 제목",
     description: "필드 상태의 설명입니다.",
-    daysAgo: 1,
   },
 };
